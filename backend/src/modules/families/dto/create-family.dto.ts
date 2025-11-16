4f925ae2fdbf3796b828d3ab2a2383fb2b6787ac
Mehdi Erdem DEDE <mehdierdemdede@gmail.com>
2025-11-17 00:29:03 +0300

    feat: add families module with dummy endpoints

---
+++ backend/src/modules/families/dto/create-family.dto.ts
@@ -0,0 +1,18 @@
+import {Type} from 'class-transformer';
+import {IsNumber, IsString, Min} from 'class-validator';
+
+export class CreateFamilyDto {
+  @IsString({message: 'Ebeveyn tam adı zorunludur.'})
+  parentFullName: string;
+
+  @IsString({message: 'Şehir bilgisi zorunludur.'})
+  city: string;
+
+  @IsString({message: 'İlçe bilgisi zorunludur.'})
+  district: string;
+
+  @Type(() => Number)
+  @IsNumber({}, {message: 'Çocuk sayısı sayı olmalıdır.'})
+  @Min(3, {message: 'Çocuk sayısı en az 3 olmalıdır.'})
+  numberOfChildren: number;
+}
